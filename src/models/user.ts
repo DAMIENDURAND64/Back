99727c55b66032de54591807a91e24578de86031
Damsbot64 <damien64durand@gmail.com>
2023-02-11 14:07:57 +0100

    few fix and input sanitize

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose, { Schema } from "mongoose";
+import escape from "html-escape";
 
 export type TUser = {
   firstName: string;
@@ -12,6 +13,17 @@ const userSchema = new Schema<TUser>({
   lastName: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
+});
+
+const sanitizeInput = (input: string): string => {
+  return escape(input);
+};
+userSchema.pre("save", function (next) {
+  this.firstName = sanitizeInput(this.firstName.toString());
+  this.lastName = sanitizeInput(this.lastName.toString());
+  this.email = sanitizeInput(this.email.toString());
+  this.password = sanitizeInput(this.password.toString());
+  next();
 });
 
 export const User = mongoose.model<TUser>("User", userSchema);
